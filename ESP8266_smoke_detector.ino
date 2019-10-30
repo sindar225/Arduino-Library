231c5ed1bb77f1030cc37bd0a7b81fd5c44df8ab
Denis Tregubov <denis_tregubov@epam.com>
2019-10-30 15:34:15 +0300

    +

--- ESP8266_smoke_detector.ino
+++ ESP8266_smoke_detector.ino
@@ -18,7 +18,7 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Gas sensor warming up!");
   //delay(20000);       // дать MQ-2 время для прогрева
-  //delay(2000);
+  //delay(2000); 
 
   // resource output example (i.e. reading a sensor value)
   thing["air_quality"] >> outputValue(getSensorValue());
@@ -29,10 +29,11 @@ void loop() {
   data["air_quality"] = getSensorValue();
   if (int(getSensorValue()) >= 300) {
     thing.call_endpoint("telegramAlert", data);
-  }
+  }  
 
   thing.handle();
-  ESP.deepSleep(20e6);
+  //delay(10000);
+  //ESP.deepSleep(20e6);
 }
 
 float getSensorValue() {
