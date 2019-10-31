e9abae8b72f00cb83aa8dd7014b013ca3f856f1d
Denis Tregubov <denis_tregubov@epam.com>
2019-10-31 12:57:51 +0300

    working

--- ESP8266_smoke_detector.ino
+++ ESP8266_smoke_detector.ino
@@ -8,32 +8,35 @@
 #define MQ2pin A0
 
 #define USERNAME "sindar225"
-#define DEVICE_ID "NODEMCU1"
-#define DEVICE_CREDENTIAL "F5590A00g_4"
+#define DEVICE_ID "Kitchen_ESP8266"
+#define DEVICE_CREDENTIAL "m5m@Rhp9H2kw"
 
 float sensorValue;  // переменная для хранения значения датчика
 ThingerSmartConfig thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL, false);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Gas sensor warming up!");
-  //delay(20000);       // дать MQ-2 время для прогрева
-  //delay(2000); 
+  //Serial.println("Gas sensor warming up!");
+  
+  thing["air_bucket"] >> [](pson &out){ 
+    out["air_quality"] = getSensorValue();
+  };
 
-  // resource output example (i.e. reading a sensor value)
   thing["air_quality"] >> outputValue(getSensorValue());
 }
 
 void loop() {
   pson data;
   data["air_quality"] = getSensorValue();
-  if (int(getSensorValue()) >= 300) {
-    thing.call_endpoint("telegramAlert", data);
-  }  
+  data["location"] = DEVICE_ID;
 
   thing.handle();
-  //delay(10000);
-  //ESP.deepSleep(20e6);
+  if (int(getSensorValue()) >= 300) {
+    Serial.print("Sending message to TG");
+    thing.call_endpoint("telegramAlert", data);
+  }  
+  thing.write_bucket("Air_quality", "air_bucket");
+  ESP.deepSleep(20e6, WAKE_RF_DEFAULT);
 }
 
 float getSensorValue() {
